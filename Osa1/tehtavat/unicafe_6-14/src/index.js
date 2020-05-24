99c7af49e92e7a447f6a5d3d555fed2f432fc6ab
MiikaM <42460214+MiikaM@users.noreply.github.com>
2020-05-24 19:50:28 +0300

    Tehtävä 1.6-11 done aka

--- Osa1/tehtavat/unicafe_6-14/src/index.js
+++ Osa1/tehtavat/unicafe_6-14/src/index.js
@@ -1,11 +1,48 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({click, text}) => (
+const Button = ({ click, text }) => (
   <button onClick={click}>
     {text}
   </button>
 )
+
+const Statistics = (props) => {
+  const good = props.goods
+  const neutral = props.neutrals
+  const bad = props.bads
+  
+  const all = good + neutral + bad
+  const average = (good + (bad * -1)) / all
+  const positive = (good / all) * 100
+
+  if (all === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={all} />
+        <StatisticLine text='average' value={average} />
+        <StatisticLine text='positive' value={positive + '%'} />
+      </tbody>
+    </table>
+  )
+}
+
+const StatisticLine = ({ value, text }) => {
+  return (
+    <tr>
+      <td>{text} {value}</td>
+    </tr>
+  )
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
@@ -13,18 +50,9 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleGood = () => {
-    setGood(good + 1)
-  }
-  
-  const handleNeutral = () => {
-    setNeutral(neutral + 1)
-  }
-  
-  const handleBad = () => {
-    setBad(bad + 1)
-  }
-
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
   return (
     <div>
       <h1>give feedback</h1>
@@ -32,9 +60,7 @@ const App = () => {
       <Button click={handleNeutral} text='Neutral' />
       <Button click={handleBad} text='Bad' />
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics goods={good} neutrals={neutral} bads={bad} />
     </div>
   )
 }
