7019c2f96ed53f1ee22ddbf54f3d01b121798e37
MiikaM <42460214+MiikaM@users.noreply.github.com>
2020-06-01 14:30:45 +0300

    Osa3.1-8 done

--- Osa3_teht/index.js
+++ Osa3_teht/index.js
@@ -1,36 +1,112 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
-let notes = [
+app.use(express.json())
+
+morgan.token('contents', function (req, res) {
+    return JSON.stringify(req.body)
+});
+
+
+app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :contents`))
+
+
+let persons = [
     {
-        id: 1,
-        content: "HTML is easy",
-        date: "2020-01-10T17:30:31.098Z",
-        important: true
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
     },
     {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2020-01-10T18:39:34.091Z",
-        important: false
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
     },
     {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2020-01-10T19:20:14.298Z",
-        important: true
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
     }
 ]
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
-  })
-  
-  app.get('/api/notes', (req, res) => {
-    res.json(notes)
-  })
-  
-  const PORT = 3001
-  app.listen(PORT, () => {
+})
+
+app.get('/info', (req, res) => {
+    const length = persons.length
+    const date = new Date()
+    console.log('length on', length)
+    res.send(`<div>
+    <p>Phonebook has info for ${length} people</p>
+    <p>${date}</p>
+    </div>`)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const note = persons.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number || body.name.length < 1 || body.number.length < 1) {
+        return response.status(400).json({
+            error: 'Name and number must be given'
+        })
+    }
+
+    const personSame = persons.filter(person => person.name === body.name)
+
+    if (personSame.length > 0) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const note = {
+        name: body.name,
+        number: body.number,
+        id: generateId
+    }
+
+    persons = persons.concat(note)
+
+    response.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+})
