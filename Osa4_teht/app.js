64afc83dbfbc3546d480dea0af88e39ef2eddda7
MiikaM <42460214+MiikaM@users.noreply.github.com>
2020-06-05 13:22:16 +0300

    Osa4.2  done

---
+++ Osa4_teht/app.js
@@ -0,0 +1,30 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+
+const mongoUrl = config.MONGODB_URI
+logger.info('Connecting to ', mongoUrl)
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('Error occured connecting to MongoDB', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.morg)
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ Osa4_teht/utils/middleware.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+morgan.token('contents', function (req) {
+  return JSON.stringify(req.body)
+})
+
+//Logs the request info
+const morg = morgan(':method :url :status :res[content-length] - :response-time ms :contents')
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'Unknown blog' })
+}
+
+
+const errorHandler = (error, request, response, next) => {
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'MongoError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  morg,
+  unknownEndpoint,
+  errorHandler
+}
