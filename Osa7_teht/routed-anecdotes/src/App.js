52f2b8e19db6ffb51b1f5b8e0c8a37bdf7a32be5
MiikaM <42460214+MiikaM@users.noreply.github.com>
2020-06-19 11:40:55 +0300

    Osa7.6 done

--- Osa7_teht/routed-anecdotes/src/App.js
+++ Osa7_teht/routed-anecdotes/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import {
   Switch, Route, Link, Redirect, useRouteMatch
 } from "react-router-dom"
+import { useField, useReset } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -51,44 +52,52 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const [redirect, setRedirect] = useState('move')
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
 
     setRedirect(null)
   }
 
+  const handleReset = (event) => {
+    event.preventDefault()
+    content.onReset()
+    author.onReset()
+    info.onReset()
+  }
 
 
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form name='createForm' onSubmit={handleSubmit} >
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input  {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input id='url' {...info} />
         </div>
-        <button >create</button>
+        <button type='submit'>create</button>
+        <button type='reset' onClick={handleReset}>reset</button>
       </form>
+
 
       <Switch>
         <Route path='/create'>
