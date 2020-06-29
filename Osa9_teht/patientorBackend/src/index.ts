0cc2bf4e9f043fb92ef727dac1e1ca57e96f6396
MiikaM <42460214+MiikaM@users.noreply.github.com>
2020-06-29 13:51:33 +0300

    Osa9.10 => initialized

--- Osa9_teht/patientorBackend/src/index.ts
+++ Osa9_teht/patientorBackend/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import patientRouter from './routes/patients';
+import diagnoseRouter from './routes/diagnoses';
 const cors = require('cors');
 const app = express();
 app.use(express.json());
@@ -10,6 +12,9 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.status(200).send('pong');
 });
+
+app.use('/api/patients', patientRouter)
+app.use('/api/diagnoses', diagnoseRouter)
   
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
