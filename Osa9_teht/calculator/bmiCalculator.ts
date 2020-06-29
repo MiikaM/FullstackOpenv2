3cbe628855dfc5e7aa61c26798c50d3fb1057300
MiikaM <42460214+MiikaM@users.noreply.github.com>
2020-06-29 10:13:07 +0300

    Osa9.3 (cmdl Bmi)

--- Osa9_teht/calculator/bmiCalculator.ts
+++ Osa9_teht/calculator/bmiCalculator.ts
@@ -1,6 +1,24 @@
+interface BmiValues {
+  height: number,
+  mass: number
+}
+
+const parseArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      mass: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
 const calculateBmi = (a: number, b: number): string => {
-  const bmi = (b / ((a/100) ** 2))
-  console.log({bmi})
+  const bmi = (b / ((a / 100) ** 2))
   if (bmi < 15) {
     return "Very severely underweight";
   } else if (bmi < 16) {
@@ -23,7 +41,8 @@ const calculateBmi = (a: number, b: number): string => {
 }
 
 try {
-  console.log(calculateBmi(180, 74));
-} catch(e) {
+  const { height, mass } = parseArguments(process.argv);
+  console.log(calculateBmi(height, mass))  
+} catch (e) {
   console.log('Error: ', e.message);
 }
